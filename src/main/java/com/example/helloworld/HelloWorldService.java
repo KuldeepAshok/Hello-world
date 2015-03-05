3707ec82f77b5cda2014d1ff3a90871165fa7b5a
KuldeepAshok <kuldeep.ashok@Kuldeep-MacBook-Pro-2.local>
2015-03-05 21:01:48 +0530

    fibonacii problem added

--- src/main/java/com/example/helloworld/HelloWorldService.java
+++ src/main/java/com/example/helloworld/HelloWorldService.java
@@ -7,10 +7,7 @@ import com.example.helloworld.core.Template;
 import com.example.helloworld.core.User;
 import com.example.helloworld.db.PersonDAO;
 import com.example.helloworld.health.TemplateHealthCheck;
-import com.example.helloworld.resources.HelloWorldResource;
-import com.example.helloworld.resources.PeopleResource;
-import com.example.helloworld.resources.PersonResource;
-import com.example.helloworld.resources.ProtectedResource;
+import com.example.helloworld.resources.Fibonacii;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.auth.basic.BasicAuthProvider;
@@ -50,18 +47,9 @@ public class HelloWorldService extends Service<HelloWorldConfiguration> {
     @Override
     public void run(HelloWorldConfiguration configuration,
                     Environment environment) throws ClassNotFoundException {
-        final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
 
         environment.addProvider(new BasicAuthProvider<User>(new ExampleAuthenticator(),
                                                             "SUPER SECRET STUFF"));
-
-        final Template template = configuration.buildTemplate();
-
-        environment.addHealthCheck(new TemplateHealthCheck(template));
-        environment.addResource(new HelloWorldResource(template));
-        environment.addResource(new ProtectedResource());
-
-        environment.addResource(new PeopleResource(dao));
-        environment.addResource(new PersonResource(dao));
+        environment.addResource(new Fibonacii());
     }
 }
--- src/main/java/com/example/helloworld/resources/Fibonacii.java
+++ src/main/java/com/example/helloworld/resources/Fibonacii.java
@@ -2,24 +2,21 @@ package com.example.helloworld.resources;
 
 import com.example.helloworld.core.User;
 import com.yammer.dropwizard.auth.Auth;
-
+import com.example.helloworld.core.CalculateFib;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.PathParam;
 
-@Path("/protected")
-public class ProtectedResource {
-    /*@GET
-    public String showSecret(@Auth User user) {
-        return String.format("Hey there, %s. You know the secret!", user.getName());
-    }*/
+@Path("/kuldeep")
+public class Fibonacii {
     @GET
     @Path("{fib}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getFibNumber(@PathParam("fib") Integer n1)
+    public Integer getFibNumber(@PathParam("fib") Integer n)
     {   
-        return "1";
+    	//System.out.println(CalculateFib.Fib(n));
+        return CalculateFib.Fib(n);
     }
 }
